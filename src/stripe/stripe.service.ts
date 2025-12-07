9ae5c300ef0248806e4e370accf2c4ee66bc1bd6
refrain62 <nao_h13@hotmail.com>
2025-12-07 12:24:31 +0900

    Stripe SDKの初期化

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import Stripe from 'stripe';
 
 @Injectable()
-export class StripeService {}
+export class StripeService {
+  private readonly stripe: Stripe
+  
+  constructor() {
+    this.stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+      apiVersion: '2020-08-27',
+    })
+  }
+}
