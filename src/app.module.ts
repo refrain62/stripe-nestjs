2fb9bb5754961fcf5b517f0209d7771081b684c0
refrain62 <nao_h13@hotmail.com>
2025-12-07 12:11:35 +0900

    yarn add @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { AppService } from './app.service';
 // モジュールの読み込み
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     // 性的HTML配信モジュールを追加し、`client`ディレクトリを読み込ませる
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
